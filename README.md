ad69ae84d0c09e7f0e195f2602e87111ed4f68f3
SnowBases <syid98@gmail.com>
2020-06-12 23:10:37 +0800

    Add weather details

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Weather-UI-Ionic-5
-Weather UI Ionic 5
+
+This project currently is work in progress. 
+Any contribution is welcome
+
+## Screensize
+
+##### Samsung Galaxy S III 
+<img width="auto" height="400px" src="https://raw.githubusercontent.com/SnowBases/Weather-UI-Ionic-5/master/screenshots/localhost_8100_home(Galaxy%20S%20III).png" alt="Samsung Galaxy S III">
+
+##### iPhone 4
+<img width="auto" height="400px" src="https://raw.githubusercontent.com/SnowBases/Weather-UI-Ionic-5/master/screenshots/localhost_8100_home(iPhone%204).png" alt="iPhone 4">
+
+##### iPhone 6/7/8 Plus
+<img width="auto" height="400px" src="https://raw.githubusercontent.com/SnowBases/Weather-UI-Ionic-5/master/screenshots/localhost_8100_home(iPhone%206_7_8%20Plus).png" alt="iPhone 6/7/8 Plus">
+
+##### iPhone X
+<img width="auto" height="400px" src="https://raw.githubusercontent.com/SnowBases/Weather-UI-Ionic-5/master/screenshots/localhost_8100_home(iPhone%20X).png" alt="iPhone X">
+
+##### iPad
+<img width="auto" height="400px" src="https://raw.githubusercontent.com/SnowBases/Weather-UI-Ionic-5/master/screenshots/localhost_8100_home(iPad).png" alt="iPad">
+
+##### iPad Pro
+<img width="auto" height="400px" src="https://raw.githubusercontent.com/SnowBases/Weather-UI-Ionic-5/master/screenshots/localhost_8100_home(iPad%20Pro).png" alt="iPad Pro">
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,14 +9,24 @@ import * as moment from 'moment';
 export class HomePage implements AfterViewInit {
   @ViewChild('country', { read: ElementRef, static: false }) countryElementRef: ElementRef;
   @ViewChild('temperature', { read: ElementRef, static: false }) temperatureElementRef: ElementRef;
-  @ViewChild('weather', { read: ElementRef, static: false }) weatherElementRef: ElementRef;
+  @ViewChild('weatherWeek', { read: ElementRef, static: false }) weatherWeekElementRef: ElementRef;
+  @ViewChild('todayWeather', { read: ElementRef, static: false }) todayWeatherElementRef: ElementRef;
 
   public weatherWeekLists = [];
   public todayWeatherDetails = [];
 
-  slideOptions = {
+  weatherWeekSlideOptions = {
     initialSlide: 0,
     slidesPerView: 5,
+    paginationClickable: true,
+    // resistance: true,
+    // resistanceRatio: 0,
+    autoplay: false
+  };
+
+  todayWeatherSlideOptions = {
+    initialSlide: 0,
+    slidesPerView: 4,
     paginationClickable: true,
     // resistance: true,
     // resistanceRatio: 0,
@@ -33,8 +43,8 @@ export class HomePage implements AfterViewInit {
       weather_state: 'Clouds',
       date: '2020-06-8'
     }, {
-      icon: 'assets/icon/cloud-sunrainy.svg',
-      weather_state: 'Rain',
+      icon: 'assets/icon/cloud-sunny.svg',
+      weather_state: 'Sunny',
       date: '2020-06-9'
     }, {
       icon: 'assets/icon/cloud-sunrainy.svg',
@@ -46,8 +56,42 @@ export class HomePage implements AfterViewInit {
       date: '2020-06-11'
     }, {
       icon: 'assets/icon/cloudy.svg',
-      weather_state: 'Clody',
+      weather_state: 'Cloudy',
       date: '2020-06-12'
+    }];
+
+    const todayWeatherDetails = [{
+      icon: 'assets/icon/cloud-plain.svg',
+      weather_state: 'Clouds',
+      time: '06:30 PM'
+    }, {
+      icon: 'assets/icon/cloud-plain.svg',
+      weather_state: 'Clouds',
+      time: '07:30 PM'
+    }, {
+      icon: 'assets/icon/cloud-plain.svg',
+      weather_state: 'Clouds',
+      time: '08:30 PM'
+    }, {
+      icon: 'assets/icon/cloud-plain.svg',
+      weather_state: 'Clouds',
+      time: '09:30 PM'
+    }, {
+      icon: 'assets/icon/cloud-plain.svg',
+      weather_state: 'Clouds',
+      time: '10:30 PM'
+    }, {
+      icon: 'assets/icon/cloud-plain.svg',
+      weather_state: 'Clouds',
+      time: '11:30 PM'
+    }, {
+      icon: 'assets/icon/cloud-plain.svg',
+      weather_state: 'Clouds',
+      time: '12:30 AM'
+    }, {
+      icon: 'assets/icon/cloud-plain.svg',
+      weather_state: 'Clouds',
+      time: '1:30 AM'
     }];
 
     const todayDate = moment().format("YYYY-MM-DD");
@@ -55,7 +99,13 @@ export class HomePage implements AfterViewInit {
       .filter(o => o.date == todayDate)
       .map(o => o.date = 'Today');
 
+    // TO-DO: Splice objects based on current time
+    // const timeNow = "06:30 PM";
+    // weatherTodayDetails
+    //   .filter(o => o.time == timeNow)
+
     this.weatherWeekLists.push(...weatherWeekLists);
+    this.todayWeatherDetails.push(...todayWeatherDetails);
   }
 
 }
